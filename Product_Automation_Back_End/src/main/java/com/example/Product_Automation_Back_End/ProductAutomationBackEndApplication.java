1c209881cd4e3756756d74a5c1d6545cc8e246f7
sanjai <arthi.a@hepl.com>
2025-04-16 17:25:14 +0530

    initial commit

---
+++ Product_Automation_Back_End/src/main/java/com/example/Product_Automation_Back_End/ProductAutomationBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Product_Automation_Back_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAutomationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAutomationBackEndApplication.class, args);
+	}
+
+}
